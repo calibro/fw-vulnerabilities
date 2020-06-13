571324ab344225f8597aa63a4eed3d8579b5c6d4
Daniele Pezzatini <daniele@bestiario.org>
2020-06-13 14:59:35 +0200

    Change range input to 1 decimal value

--- src/components/filters/RangeFilter.vue
+++ src/components/filters/RangeFilter.vue
@@ -2,11 +2,11 @@
   <div class="range-filter">
     <div class="input-block">
       <label>Min</label>
-      <b-form-input ref="minInput" :value="minVal" type="number" :min="minRange" :max="Math.min(maxRange, maxVal)" @change="change" step="0.01"></b-form-input>
+      <b-form-input ref="minInput" :value="minVal" type="number" :min="minRange" :max="Math.min(maxRange, maxVal)" @change="change" step="0.1"></b-form-input>
     </div>
     <div class="input-block">
       <label>Max</label>
-      <b-form-input ref="maxInput" :value="maxVal" type="number" :min="Math.max(minRange,minVal)" :max="maxRange" @change="change" step="0.01"></b-form-input>
+      <b-form-input ref="maxInput" :value="maxVal" type="number" :min="Math.max(minRange,minVal)" :max="maxRange" @change="change" step="0.1"></b-form-input>
     </div>
   </div>
 </template>
@@ -23,16 +23,16 @@ export default {
       return this.$store.state.data.filters[this.filterKey]
     },
     minRange () {
-      return this.range && this.range.length == 2 ? this.range[0].toFixed(2) : 0
+      return this.range && this.range.length == 2 ? this.range[0].toFixed(1) : 0
     },
     maxRange () {
-      return this.range && this.range.length == 2 ? this.range[1].toFixed(2) : 10
+      return this.range && this.range.length == 2 ? this.range[1].toFixed(1) : 10
     },
     minVal () {
-      return this.value && this.value.length == 2 ? this.value[0].toFixed(2) : 0
+      return this.value && this.value.length == 2 ? this.value[0].toFixed(1) : 0
     },
     maxVal () {
-      return this.value && this.value.length == 2 ? this.value[1].toFixed(2) : 10
+      return this.value && this.value.length == 2 ? this.value[1].toFixed(1) : 10
     }
   },
   methods: {
