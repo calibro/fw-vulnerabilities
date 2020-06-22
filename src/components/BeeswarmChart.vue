55082ea97a1f0f6816dd29bbd259f4431c73e284
uf0 <scnepa222@gmail.com>
2020-06-22 18:14:31 +0200

    close #10

--- src/components/BeeswarmChart.vue
+++ src/components/BeeswarmChart.vue
@@ -256,11 +256,12 @@ export default {
             .attr("font-size", "16px")
             .attr("font-family", "Arial, sans-serif")
             .attr("font-weight", "bold")
-            .attr("x", -margin.left)
+            .attr("transform", `translate(${-margin.left},0)`)
             .attr("text-anchor", "start")
             .text(d => d + ` (${groups.get(d)})`);
 
           g.selectAll(".domain, .tick line").attr("stroke", "#ddd");
+          g.selectAll(".tick text").call(self.wrap, margin.left);
         });
 
         let yAxisHelperGroup = svg.select("#yAxisHelperGroup");
@@ -508,6 +509,42 @@ export default {
           minY +
           labelSize * 2}`
       );
+    },
+    wrap(text, width) {
+      text.each(function() {
+        var text = d3.select(this),
+          words = text
+            .text()
+            .split(/\s+/)
+            .reverse(),
+          word,
+          line = [],
+          lineNumber = 0,
+          lineHeight = 1.1, // ems
+          y = text.attr("y"),
+          dy = parseFloat(text.attr("dy")),
+          tspan = text
+            .text(null)
+            .append("tspan")
+            .attr("x", 0)
+            .attr("y", y)
+            .attr("dy", dy + "em");
+        while ((word = words.pop())) {
+          line.push(word);
+          tspan.text(line.join(" "));
+          if (tspan.node().getComputedTextLength() > width) {
+            line.pop();
+            tspan.text(line.join(" "));
+            line = [word];
+            tspan = text
+              .append("tspan")
+              .attr("x", 0)
+              .attr("y", y)
+              .attr("dy", `${++lineNumber * lineHeight + dy}em`)
+              .text(word);
+          }
+        }
+      });
     },
     toggleAnnotation(item, node) {
       let annIndex = this.annotations.findIndex(ann =>
